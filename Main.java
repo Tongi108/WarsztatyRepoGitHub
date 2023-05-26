f8624edbaf3a35c2152f625bff42114e5698283d
Repo2 <biuro.project108@gmail.com>
2023-05-26 22:08:25 +0200

    commit1: from repo2

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hari from repo2");
+        System.out.println("Hari from repo2 part2");
         System.out.println("Hari from repo1");
         System.out.println("Hari from repo1 part2");
     }
